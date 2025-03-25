c72ca257145106fa2d2e8b0bf2f8104bd9118698
Chadwick Koo <chadwickkoo@outlook.com>
2025-03-25 10:45:05 +0300

    Modified index.js file

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name, email){
+    return fetch("http://localhost:3000/users",{
+        method : 'POST',
+        headers :{
+            "content-type" : "application/json",
+                "Accept" : "application/json",
+        },
+        body: JSON.stringify({
+            name : name,
+            email : email
+        })    
+    })
+    .then(res => res.json())
+    .then(user => {
+        let userId = document.createElement("p")
+        userId.innerHTML = `id ${user.id}`
+        document.body.appendChild(userId)
+            })
+    .catch(error => {
+        let errorElement = document.createElement("p");
+        errorElement.innerHTML = `Error: ${error.message}`;  
+        document.body.appendChild(errorElement);     
+    })
+}
+
+
+
+
+
